5305b0df5f233e7c286d6b049868d346a3a9f1f2
Rodrigo Escobar <r.escobarl@uniandes.edu.co>
2024-03-02 23:06:39 -0500

    Add register token logic for app startup by env variable

--- mobile_app/src/hooks/usePushNotification.tsx
+++ mobile_app/src/hooks/usePushNotification.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import messaging from '@react-native-firebase/messaging';
-import {PermissionsAndroid, Platform} from 'react-native';
+import React from "react";
+import messaging from "@react-native-firebase/messaging";
+import { PermissionsAndroid, Platform } from "react-native";
+import { v4 as uuidv4 } from "uuid";
 
 const usePushNotification = () => {
   const requestUserPermission = async () => {
-    if (Platform.OS === 'ios') {
+    if (Platform.OS === "ios") {
       //Request iOS permission
       const authStatus = await messaging().requestPermission();
       const enabled =
@@ -12,55 +13,78 @@ const usePushNotification = () => {
         authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
       if (enabled) {
-        console.log('Authorization status:', authStatus);
+        console.log("Authorization status:", authStatus);
       }
-    } else if (Platform.OS === 'android') {
+    } else if (Platform.OS === "android") {
       //Request Android permission (For API level 33+, for 32 or below is not required)
       const res = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
       );
     }
-  }
+  };
 
   const getFCMToken = async () => {
     const fcmToken = await messaging().getToken();
     if (fcmToken) {
-      console.log('Your Firebase Token is:', fcmToken);
+      console.log("Your Firebase Token is:", fcmToken);
     } else {
-      console.log('Failed', 'No token received');
+      console.log("Failed", "No token received");
     }
+
+    await updateDeviceTokenAPI(fcmToken);
+  };
+
+  const updateDeviceTokenAPI = async (fcmToken: string) => {
+    const alertsBasePath = process.env.REACT_APP_API_BASE_PATH;
+    if (!alertsBasePath) {
+      conole.log("Skipping device token registration, API base path not found");
+      return;
+    }
+
+    const apiUrl = `${alertsBasePath}/alerts/register-device`;
+    await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        user_id: uuidv4(),
+        device_token: fcmToken,
+        enabled: true
+      })
+    });
   };
 
   const listenToForegroundNotifications = async () => {
     const unsubscribe = messaging().onMessage(async remoteMessage => {
       console.log(
-        'A new message arrived! (FOREGROUND)',
-        JSON.stringify(remoteMessage),
+        "A new message arrived! (FOREGROUND)",
+        JSON.stringify(remoteMessage)
       );
     });
     return unsubscribe;
-  }
+  };
 
   const listenToBackgroundNotifications = async () => {
     const unsubscribe = messaging().setBackgroundMessageHandler(
       async remoteMessage => {
         console.log(
-          'A new message arrived! (BACKGROUND)',
-          JSON.stringify(remoteMessage),
+          "A new message arrived! (BACKGROUND)",
+          JSON.stringify(remoteMessage)
         );
-      },
+      }
     );
     return unsubscribe;
-  }
+  };
 
   const onNotificationOpenedAppFromBackground = async () => {
     const unsubscribe = messaging().onNotificationOpenedApp(
       async remoteMessage => {
         console.log(
-          'App opened from BACKGROUND by tapping notification:',
-          JSON.stringify(remoteMessage),
+          "App opened from BACKGROUND by tapping notification:",
+          JSON.stringify(remoteMessage)
         );
-      },
+      }
     );
     return unsubscribe;
   };
@@ -68,8 +92,8 @@ const usePushNotification = () => {
   const onNotificationOpenedAppFromQuit = async () => {
     const message = await messaging().getInitialNotification();
 
-    if(message) {
-      console.log('App opened from QUIT by tapping notification:', JSON.stringify(message));
+    if (message) {
+      console.log("App opened from QUIT by tapping notification:", JSON.stringify(message));
     }
   };
 
@@ -79,7 +103,7 @@ const usePushNotification = () => {
     listenToForegroundNotifications,
     listenToBackgroundNotifications,
     onNotificationOpenedAppFromBackground,
-    onNotificationOpenedAppFromQuit,
+    onNotificationOpenedAppFromQuit
   };
 };
 
